59a460dad6c7e4d252faa2aa7dbf183afdaffef5
Satyaarth <satyarthchhabra18@gmail.com>
2021-06-13 23:57:21 +0530

    minor change

--- src/components/theme/themeProvider.jsx
+++ src/components/theme/themeProvider.jsx
@@ -1,7 +1,7 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
 const arcBlue = "#00dbd0";
-const arcOrange = "#FFBA60";
+
 const lightGrey = "#374b5c";
 
 export const theme = createMuiTheme({
