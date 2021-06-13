bfb6140a3f2539782d597502a4cba18325298803
Satyaarth <satyarthchhabra18@gmail.com>
2021-06-13 23:57:42 +0530

    abc

--- src/components/Presented/Presented.jsx
+++ src/components/Presented/Presented.jsx
@@ -25,7 +25,7 @@ const breakPoints = [
 const Presented = () => {
     const classes = useStyles();
     // eslint-disable-next-line no-array-constructor
-    const uselessArray = new Array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20");
+    const uselessArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 
 
 
